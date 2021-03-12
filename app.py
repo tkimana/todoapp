52abb7bea1bb9eb53ecd874ff968830820377b7c
tkimana <kimana.theodore@gmail.com>
2021-03-12 15:54:52 -0600

    added changes

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request
 
 app= Flask(__name__)
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-
-if __name__=='__name__':
+if __name__== '__name__':
     app.debug=True
     app.run()
