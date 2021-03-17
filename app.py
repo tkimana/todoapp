30e4a7b79db4efedd50f88add329a7f805da7c79
tkimana <kimana.theodore@gmail.com>
2021-03-17 16:42:01 -0500

    added changes

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app= Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+
+
 if __name__== '__name__':
-    app.debug=True
+    db.create_all()
     app.run()
